73f4387a268691cf57987dfcf534f0e9d9629279
Ganesh Gunasegaran <me@itsgg.com>
2020-09-13 13:20:49 +0530

    Added chain syncronization logic

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 const Blockchain = require("./blockchain");
-const PORT = 3000;
+const PubSub = require("./pubsub");
+
+const DEFAULT_PORT = 3000;
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
+
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+setTimeout(() => {
+  pubsub.broadcastChain();
+}, 1000);
 
 app.use(bodyParser.json());
 
@@ -15,9 +25,34 @@ app.get("/api/blocks", (req, res) => {
 app.post("/api/mine", (req, res) => {
   const { data } = req.body;
   blockchain.addBlock({ data });
+  pubsub.broadcastChain();
   res.redirect("/api/blocks");
 });
 
+const syncChains = () => {
+  request(
+    {
+      url: `${ROOT_NODE_ADDRESS}/api/blocks`,
+    },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+        console.log('replace chain on a sync with', rootChain);
+        blockchain.replace(rootChain);
+      }
+    }
+  );
+};
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 10000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+
 app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
+  syncChains();
 });
--- pubsub.js
+++ pubsub.js
@@ -1,23 +1,47 @@
 const redis = require("redis");
 
 const CHANNELS = {
-  TEST: "TEST",
+  BLOCKCHAIN: "BLOCKCHAIN",
 };
 
 class PubSub {
-  constructor() {
+  constructor({ blockchain }) {
+    this.blockchain = blockchain;
     this.publisher = redis.createClient();
     this.subscriber = redis.createClient();
 
-    this.subscriber.subscribe(CHANNELS.TEST);
-    this.subscriber.on("message", this.handleMessage);
+    this.subscribeToChannels();
+
+    this.subscriber.on("message", (channel, message) =>
+      this.handleMessage(channel, message)
+    );
+  }
+
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach((channel) => {
+      this.subscriber.subscribe(channel);
+    });
   }
 
   handleMessage(channel, message) {
     console.log(`Message received. Channel: ${channel}, Message: ${message}`);
+    const parsedMessage = JSON.parse(message);
+
+    if (channel === CHANNELS.BLOCKCHAIN) {
+      this.blockchain.replace(parsedMessage);
+    }
+  }
+
+  publish({ channel, message }) {
+    this.publisher.publish(channel, message);
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain),
+    });
   }
 }
 
-const testps = new PubSub();
-
-setTimeout(() => testps.publisher.publish(CHANNELS.TEST, "Foooooo"), 3000);
+module.exports = PubSub;
